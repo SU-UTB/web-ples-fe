f19b30085596aa9f42674c1183e1555fcdde64e0
WilloS88 <ramon2003@email.cz>
2024-01-23 22:35:18 +0100

    Addition to tickets section

--- src/components/sections/TicketsSection.tsx
+++ src/components/sections/TicketsSection.tsx
@@ -1,10 +1,36 @@
-import { TicketDiv } from "../TicketDiv"
+import { TicketDiv } from "../divs/TicketDiv"
+import { ContactDiv } from "../divs/ContactDiv"
+import { InfoCard } from "../divs/InfoCard"
 
 export const TicketsSection = () => {
   return (
-    <div className="flex flex-col items-center mb-10">
-      <div className="uppercase font-bold text-xl mb-4">Vstupenky</div>
-      <div className="flex">
+    <div className="flex flex-col items-center mt-10 ">
+      <div className="grid grid-cols-3 grid-rows-1 justify-center items-center">
+        <div className="px-8 col-start-1 col-end-3">
+          <div className="uppercase font-bold text-4xl mb-4 font-numbers">
+            Vstupenky
+          </div>
+          <div className="text-lg ">
+            Srdečně vás zveme na XXII. ročník Reprezentačního plesu Univerzity
+            Tomáše Bati ve Zlíně. Již 16. 2. 2024 se v Kongresovém centru ve
+            Zlíně společně přeneseme na Olymp a poznáme, jaké to je, KDYŽ BOHOVÉ
+            TANČÍ. Ať už jste obyčejný smrtelník, polobůh nebo samotný Zeus, o
+            ples pod Olympem určitě nesmíte přijít! Aktuální dění a novinky
+            můžete sledovat na Facebookové události.
+          </div>
+        </div>
+        <div className="max-w-md px-8 col-start-3 col-end-4">
+          <InfoCard headline="Bližší informace k předprodeji:">
+            <ContactDiv
+              name="Pavlína Šromotová"
+              position=""
+              email="sromotova@sutb.cz"
+              phone="+421 918 234 777"
+            />
+          </InfoCard>
+        </div>
+      </div>
+      <div className="flex justify-center items-center mb-10">
         <TicketDiv
           img="/tickets/Stani1.png"
           alt="Stání"
