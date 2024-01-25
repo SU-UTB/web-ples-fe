1d156f3049119a832672ad3b5d0eaab34ba27da0
WilloS88 <ramon2003@email.cz>
2024-01-25 21:39:34 +0100

    Implementation of new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import HeroSection from "./components/sections/HeroSection"
 import AboutSection from "./components/sections/AboutSection"
+import { MenuComponent } from "./components/hamburger/MenuComponent"
 import { TicketsSection } from "./components/sections/TicketsSection"
 import { SupportProgramSection } from "./components/sections/SupportProgramSection"
 import { GallerySection } from "./components/sections/GallerySection"
 import { ContactSection } from "./components/sections/ContactSection"
 import { PartnerSection } from "./components/sections/PartnerSection"
-import { MenuComponent } from "./components/hamburger/MenuComponent"
+import { OrganizerSection } from "./components/sections/OrganizerSection"
+import { FooterSection } from "./components/sections/FooterSection"
 
 import "./index.css"
 
@@ -20,6 +22,8 @@ function App() {
       <GallerySection />
       <ContactSection />
       <PartnerSection />
+      <OrganizerSection />
+      <FooterSection />
     </main>
   )
 }
