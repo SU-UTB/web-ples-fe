1a1283f317c0ba6d6a9cc3485751050998a5bec7
Tomáš Juřička <tomas.juricka@outlook.cz>
2024-03-13 13:55:14 +0100

    Partners

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,12 +2,13 @@ import { MenuComponent } from '../hamburger/MenuComponent';
 import { AboutSection } from '../sections/AboutSection';
 import { ContactSection } from '../sections/ContactSection';
 import { GallerySection } from '../sections/GallerySection';
+import { GeneralPartnerSection } from '../sections/GeneralPartnerSection';
 import { HeroSection } from '../sections/HeroSection';
+import { MainPartnerSection } from '../sections/MainPartnerSection';
 import { OrganizerSection } from '../sections/OrganizerSection';
+import { PartnersSection } from '../sections/PartnersSection';
 import { SupportProgramSection } from '../sections/SupportProgramSection';
 import { TicketsSection } from '../sections/TicketsSection';
-import { GeneralPartnerSection } from '../sections/GeneralPartnerSection';
-import { MainPartnerSection } from '../sections/MainPartnerSection';
 import { Footer } from '../shared/Footer';
 
 export const HomePage = () => (
@@ -22,6 +23,7 @@ export const HomePage = () => (
       <ContactSection />
       <GeneralPartnerSection />
       <MainPartnerSection />
+      <PartnersSection />
       <OrganizerSection />
     </main>
     <Footer />
