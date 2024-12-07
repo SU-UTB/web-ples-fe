c9816208e6fab999ccd35d7c49fb1daba54506f1
Pavos21 <metalmasterboy@gmail.com>
2024-12-07 12:54:17 +0100

    Organizer redirect links

--- src/components/sections/OrganizerSection.tsx
+++ src/components/sections/OrganizerSection.tsx
@@ -17,19 +17,24 @@ export const OrganizerSection = () => {
   };
 
   const handleClick = () => {
-    clickCount.current += 1;
+      clickCount.current += 1;
 
-    if (timer.current) {
-      clearTimeout(timer.current);
-    }
-
-    timer.current = setTimeout(() => {
-      if (clickCount.current === 3) {
-        handleTripleClick();
+      if (timer.current) {
+          clearTimeout(timer.current);
       }
-      clickCount.current = 0; // Reset click count after a short delay
-    }, 400); // Adjust the delay as needed
+
+      timer.current = setTimeout(() => {
+          if (clickCount.current === 3) {
+              handleTripleClick();
+          } else if (clickCount.current === 1) {
+              // Open a _blank link if it's a single click
+              window.open('https://su.utb.cz', '_blank');
+          }
+
+          clickCount.current = 0; // Reset click count after a short delay
+      }, 300); // Adjust the delay as needed
   };
+
 
   return (
     <div style={{
@@ -56,14 +61,22 @@ export const OrganizerSection = () => {
               style={{
                 transition: 'transform 1s ease-in-out',
                 transform: isRolling ? 'rotate(720deg) scale(1.5)' : 'rotate(0deg) scale(1)', // Apply barrel roll rotation
+                cursor: 'pointer'
               }}
             />
-            <img
-              src={utbIcon}
-              loading="lazy"
-              alt="Univerzita Tomáše Bati"
-              className="h-6 sm:h-8"
-            />
+            <a 
+              href="https://www.utb.cz" 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              <img
+                src={utbIcon}
+                loading="lazy"
+                alt="Univerzita Tomáše Bati"
+                className="h-6 sm:h-8"
+              />
+            </a>
+
           </div>
         </div>
       </Section>
