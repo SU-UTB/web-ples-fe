7ca214d3a0d0836eeb1fff1500cac9862c2c33f1
Pavos21 <metalmasterboy@gmail.com>
2024-12-28 00:47:58 +0100

    Sticky image in AboutSection

--- src/components/sections/AboutSection.tsx
+++ src/components/sections/AboutSection.tsx
@@ -8,17 +8,25 @@ import program from "../../assets/img/headlines/program.svg"
 
 export const AboutSection = () => {
   return (
-    <div style={{
-      backgroundColor: '#F3E7E7'
-    }}>
+    <div
+      style={{
+        backgroundColor: '#F3E7E7',
+      }}
+    >
       <Section id="o-plese" className="">
-        <div className="flex flex-col items-center justify-center lg:items-start lg:gap-x-4 lg:flex-row">
+        <div className="flex flex-col items-center justify-center lg:items-start lg:gap-x-4 lg:flex-row relative">
           <img
             src={aboutImg}
             alt="Socha"
             width={645}
             height={645}
             className="w-full max-w-[645px] h-auto hidden desktop:block"
+            style={{
+              position: 'sticky',
+              top: '0px', // Sticks to the viewport top
+              alignSelf: 'flex-start', // Aligns the image to the section top
+              zIndex: 10, // Ensures the image stays above other elements
+            }}
           />
           <div className="flex flex-col gap-8">
             <AboutCard />
