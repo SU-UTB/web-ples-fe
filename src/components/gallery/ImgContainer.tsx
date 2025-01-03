83850e949ee19284d171a37662c8adc885b4f3e9
Pavos21 <metalmasterboy@gmail.com>
2025-01-03 19:00:33 +0100

    Removed hover in gallery

--- src/components/gallery/ImgContainer.tsx
+++ src/components/gallery/ImgContainer.tsx
@@ -1,38 +1,18 @@
 import { PhotoType } from '../../AppTypes';
 import { GALLERY_PHOTO_HEIGHT } from '../../utils/constants';
-import { useState } from 'react';
 
 export const ImgContainer = ({ src, alt, width, height }: PhotoType) => {
-  const [isHovered, setIsHovered] = useState(false);
-
-  const handleTouchStart = () => {
-    setIsHovered(true);
-  };
-
-  const handleTouchEnd = () => {
-    setIsHovered(false);
-  };
-
   return (
     <div
-      className="relative flex-auto overflow-hidden rounded-10 group"
+      className="relative flex-auto overflow-hidden rounded-10"
       style={{ height: GALLERY_PHOTO_HEIGHT }}
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      onTouchStart={handleTouchStart}
-      onTouchEnd={handleTouchEnd}
-      onTouchCancel={handleTouchEnd} // Important for interrupted touches
     >
-      <div
-        className="absolute inset-0 bg-[#B22547] transition duration-500"
-        style={{ opacity: isHovered ? 0 : 0.4 }}
-      ></div>
       <img
         src={src}
         alt={alt}
         width={width}
         height={height}
-        className="object-cover w-full h-full align-middle transition duration-500"
+        className="object-cover w-full h-full align-middle"
         loading="lazy"
       />
     </div>
