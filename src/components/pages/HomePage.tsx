74c64c9e34bde1b2451d3de39dc1d3e111930bc1
Jan Bures <buress.jan@email.cz>
2025-01-07 19:30:34 +0100

    make: Use yarn instead of npm

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -2,12 +2,8 @@ import { MenuComponent } from '../hamburger/MenuComponent';
 import { AboutSection } from '../sections/AboutSection';
 import { ContactSection } from '../sections/ContactSection';
 import { GallerySection } from '../sections/GallerySection';
-import { GeneralPartnerSection } from '../sections/GeneralPartnerSection';
 import { HeroSection } from '../sections/HeroSection';
-import { MainPartnerSection } from '../sections/MainPartnerSection';
 import { OrganizerSection } from '../sections/OrganizerSection';
-import { PartnersSection } from '../sections/PartnersSection';
-import { SupportProgramSection } from '../sections/SupportProgramSection';
 import { TicketsSection } from '../sections/TicketsSection';
 import { Footer } from '../shared/Footer';
 
@@ -18,12 +14,8 @@ export const HomePage = () => (
       <HeroSection />
       <AboutSection />
       <TicketsSection />
-      {/* <SupportProgramSection /> */}
       <GallerySection />
       <ContactSection />
-      {/* <GeneralPartnerSection /> */}
-      {/* <MainPartnerSection /> */}
-      {/* <PartnersSection /> */}
       <OrganizerSection />
     </main>
     <Footer />
