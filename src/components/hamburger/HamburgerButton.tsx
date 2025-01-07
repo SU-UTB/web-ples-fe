3d70e77bc276ee1cf68c7904e1fda92bdf05163b
Jan Bures <buress.jan@email.cz>
2025-01-07 19:09:31 +0100

    refactor: Hamburger - layout

--- src/components/hamburger/HamburgerButton.tsx
+++ src/components/hamburger/HamburgerButton.tsx
@@ -8,11 +8,11 @@ type HamburgerButtonProps = {
 export const HamburgerButton = ({ toggle, isOpen }: HamburgerButtonProps) => (
   <button
     onClick={toggle}
-    className="fixed z-50 top-5 right-5 h-[40px] w-[40px] rounded-[10px] shadow-card md:h-[60px] md:w-[60px]"
-    style={{backgroundColor: '#F3E7E7'}}
+    className="fixed flex justify-center items-center z-50 top-5 right-5 h-[40px] w-[40px] rounded-[10px] shadow-card md:h-[60px] md:w-[60px]"
+    style={{ backgroundColor: '#F3E7E7' }}
   >
     <motion.div
-      className="w-full h-full ps-2.5 pt-2.5 space-y-2 md:ps-4 md:pt-4 md:space-y-3"
+      className="flex flex-col justify-between w-[50%] h-[50%] space-y-2 md:space-y-3 "
       initial={false}
       animate={isOpen ? 'open' : 'closed'}
     >
@@ -22,30 +22,32 @@ export const HamburgerButton = ({ toggle, isOpen }: HamburgerButtonProps) => (
           open: {
             rotate: 45,
             translateY: window.innerWidth >= 768 ? 14 : 8, // Adjust Y translation for desktop (>=768px)
-            translateX: 0,  
+            translateX: 0,
           },
         }}
-        className="w-5 h-[1.5px] md:w-8 md:h-[2px] rounded-full"
-        style={{backgroundColor: '#02264F'}}
+        className="w-full h-[1px] md:h-[2px]"
+        style={{ backgroundColor: '#02264F' }}
       />
       <motion.div
         variants={{
           closed: { opacity: 1 },
           open: { opacity: 0 },
         }}
-        className="w-5 h-[1.5px] md:w-8 md:h-[2px] rounded-full"
-        style={{backgroundColor: '#02264F'}}
+        className="w-full h-[1px] md:h-[2px]"
+        style={{ backgroundColor: '#02264F' }}
         transition={{ duration: 0.1 }}
       />
       <motion.div
         variants={{
           closed: { rotate: 0, translateY: 0 },
-          open: { rotate: -45,
+          open: {
+            rotate: -45,
             translateY: window.innerWidth >= 768 ? -13 : -10,
-            translateX: 0 },
+            translateX: 0,
+          },
         }}
-        className="w-5 h-[1.5px] md:w-8 md:h-[2px] rounded-full"
-        style={{backgroundColor: '#02264F'}}
+        className="w-full h-[1px] md:h-[2px]"
+        style={{ backgroundColor: '#02264F' }}
       />
     </motion.div>
   </button>
