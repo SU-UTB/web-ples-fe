db214301668d053733811ab9de9448b73a6345d1
Pavel Krůček <94629483+Pavos21@users.noreply.github.com>
2026-01-19 10:26:26 +0100

    Update sale date in contstants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,3 @@
 export const COUNTDOWN_DATE = new Date('2026-02-20T19:00:00');
-export const TICKET_BUY = new Date('2026-01-23T09:00:00+01:00');
+export const TICKET_BUY = new Date('2026-01-22T09:00:00+01:00');
 export const GALLERY_PHOTO_HEIGHT = 363;
